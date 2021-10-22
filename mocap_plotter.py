bca1d12cfcc01927e8f8ecbbb5b4459e730c6dcd
aurelappius <appiusa@student.ethz.ch>
2021-10-22 10:11:20 +0200

    Added Flight Data and improved mocap plotter

--- mocap_plotter.py
+++ mocap_plotter.py
@@ -6,16 +6,19 @@ import os
 import csv
 import statistics
 
-#fig_proj = plt.title("3D Projection")
+################################################# Parameters
+Start = 0 #start frame
+N = 10000 #how many frames
+Framerate = 100
+POSMAX = [2000,2000,2000] #maximal allowed xyz values>
+show_square_ref = 1 #set 0 to hide, 1 to show
+delete_start_land = 0 #set 0 to keep, 1 to delete
+ref_color = "black" #reference color
+curve_color = "red" #measurement color
+################################################# Plot initialization
 fig_proj = plt.figure() #projected plot
-
-#fig_xyz = plt.title("XYZ Projection")
 fig_xyz = plt.figure() #xyz plot
-
-#fig_rpy = plt.title("Roll,Pitch,Yaw Projection")
 fig_rpy = plt.figure() #roll,pitch,yaw plot
-
-
 proj_plt = fig_proj.add_subplot(111, projection='3d')
 x_plt = fig_xyz.add_subplot(311)
 y_plt = fig_xyz.add_subplot(312)
@@ -23,73 +26,81 @@ z_plt = fig_xyz.add_subplot(313)
 rol_plt = fig_rpy.add_subplot(311)
 pit_plt = fig_rpy.add_subplot(312)
 yaw_plt = fig_rpy.add_subplot(313)
-header_list = ["Frame", "Framerate", "Latency", "x", "y", "z", "roll", "pitch", "yaw","err"]
-pathToData = "data.csv"
-################################################# Command line arguments
+################################################# Data initialization / Read Data
+header_list = ["Frame", "Framerate", "Latency", "x", "y", "z", "roll", "pitch", "yaw","err"] #plot data layout
+pathToData = "Flight_Data/10_21_square_good.csv" #standard data Path if there is no argument given
 if len(sys.argv)==2:
 	pathToData = sys.argv[1]
-print("Loading " + pathToData + " ...")
-################################################# Parameters
-Start = 11300 #start frame
-N = 750 #how many frames
-Framerate = 100
-POSMAX = [2000,2000,2000] #maximal allowed xyz values>
-
-################################################# Read Data
 FRA = pd.read_csv(pathToData, usecols=["Frame"],skiprows=Start,nrows=N, names=header_list).to_numpy()
 ZeroFrame = FRA[0,0]
-Time = (FRA - FRA[0,0])/Framerate
+Time = (FRA - ZeroFrame)/Framerate
 RPY = pd.read_csv(pathToData, usecols=["roll", "pitch", "yaw"],skiprows=Start,nrows=N, names=header_list).to_numpy()
 POS = pd.read_csv(pathToData, usecols=["x", "y", "z"],skiprows=Start,nrows=N, names=header_list).to_numpy()
 
-################################################# Prepare Data
-#delete outlier
-d=[0]
-for i in range(0,len(POS[:,0])):
-	if abs(POS[i,0]) > POSMAX[0] or abs(POS[i,1]) > POSMAX[1] or abs(POS[i,2]) > POSMAX[2]:
-		d = np.append(i,d)
-POS=np.delete(POS, d ,0)
-RPY=np.delete(RPY, d ,0)
-Time=np.delete(Time, d ,0)
-#calculate mean 
-MEAN = [statistics.mean(POS[:,0]), statistics.mean(POS[:,1]), statistics.mean(POS[:,2])]
+################################################# Prepare / Manipulate Data
+#delete outlier #abs(POS[i,0]) > POSMAX[0] or abs(POS[i,1]) > POSMAX[1] or abs(POS[i,2]) > POSMAX[2]:
+#delete start/land part
+if delete_start_land:
+	d=[0]
+	for i in range(0,len(POS[:,0])):
+		if abs(POS[i,2])<1970:
+			d = np.append(i,d)
+	POS=np.delete(POS, d ,0)
+	RPY=np.delete(RPY, d ,0)
+	Time=np.delete(Time, d ,0)
+#set offset for position data [x,y,z]
+Offset = [0,0,0] #[statistics.mean(POS[:,0]), statistics.mean(POS[:,1]), statistics.mean(POS[:,2])]
+
+################################################# Data for trajectory
+if show_square_ref:
+	#square
+	lin_0_1000 = np.linspace(0,1000, num=1000)
+	con_0 = np.full(1000,0)
+	con_1000 = np.full(1000,1000)
+	#proj data
+	X_t = np.concatenate( (lin_0_1000, con_1000, lin_0_1000, con_0) )
+	Y_t = np.concatenate( (con_0, -lin_0_1000, -con_1000, -lin_0_1000) )
+	Z_t = np.full(4000, 2000)
+	#xyz data
+	tx_P = np.concatenate( (np.full(500,0), np.linspace(0,1000, num=500), np.full(1000,1000), np.linspace(0,1000, num=500), np.full(1000,0)) )	
+	tx_t = np.concatenate( (np.linspace(10,15, num=500), np.full(500,15), np.linspace(15,25, num=1000), np.full(500,25), np.linspace(25,35, num=1000)) )
+	ty_P = np.concatenate( (np.full(1000,0), np.linspace(0,-1000, num=500), np.full(1000,-1000), np.linspace(0,-1000, num=500), np.full(500,0)) )
+	ty_t = np.concatenate( (np.linspace(10,20, num=1000), np.full(500,20), np.linspace(20,30, num=1000), np.full(500,30), np.linspace(30,35, num=500)) )
+	tz_P = np.full(1000,2000)
+	tz_t = np.linspace(10,35, num=1000)
+	#plot
+	proj_plt.scatter(X_t,Y_t,Z_t,marker='o',s=1, c=ref_color)
+	x_plt.scatter(tx_t,tx_P,marker='o',s=1, c=ref_color)
+	y_plt.scatter(ty_t,ty_P,marker='o',s=1, c=ref_color)
+	z_plt.scatter(tz_t,tz_P,marker='o',s=1, c=ref_color)
 
 ################################################# Plot X,Y,Z
-x_plt.scatter(Time,POS[:,0]-MEAN[0],marker='o',s=1)
+x_plt.scatter(Time,POS[:,0]-Offset[0],marker='o',s=1, c=curve_color)
 x_plt.set_xlabel('time [s]')
-x_plt.set_ylabel('X [m]')
-y_plt.scatter(Time,POS[:,1]-MEAN[1],marker='o',s=1)
+x_plt.set_ylabel('X [mm]')
+y_plt.scatter(Time,POS[:,1]-Offset[1],marker='o',s=1, c=curve_color)
 y_plt.set_xlabel('time [s]')
-y_plt.set_ylabel('Y [m]')
-z_plt.scatter(Time,POS[:,2]-MEAN[2],marker='o',s=1)
+y_plt.set_ylabel('Y [mm]')
+z_plt.scatter(Time,POS[:,2]-Offset[2],marker='o',s=1, c=curve_color)
 z_plt.set_xlabel('time [s]')
-z_plt.set_ylabel('Z [m]')
-
-plt.savefig(pathToData.split(".")[0] + "_xyz.png")
-print("Saving " + pathToData.split(".")[0] + "_xyz.png ...")
-
-
+z_plt.set_ylabel('Z [mm]')
 
 ################################################# Plot Roll,Pitch, Yaw
-rol_plt.scatter(Time,RPY[:,0],marker='o',s=1)
+rol_plt.scatter(Time,RPY[:,0],marker='o',s=1, c=curve_color)
 rol_plt.set_xlabel('time [s]')
 rol_plt.set_ylabel('roll [deg]')
-pit_plt.scatter(Time,RPY[:,1],marker='o',s=1)
+pit_plt.scatter(Time,RPY[:,1],marker='o',s=1, c=curve_color)
 pit_plt.set_xlabel('time [s]')
 pit_plt.set_ylabel('pitch [deg]')
-yaw_plt.scatter(Time,RPY[:,2],marker='o',s=1)
+yaw_plt.scatter(Time,RPY[:,2],marker='o',s=1, c=curve_color)
 yaw_plt.set_xlabel('time [s]')
 yaw_plt.set_ylabel('yaw [deg]')
 
 ################################################# Plot Projection
-proj_plt.scatter(POS[:,0]-MEAN[0],POS[:,1]-MEAN[1],POS[:,2]-MEAN[2],marker='o',s=1)
-proj_plt.set_xlabel("X [m]")
-proj_plt.set_ylabel("Y [m]")
-proj_plt.set_zlabel("Z [m]")
+proj_plt.scatter(POS[:,0]-Offset[0],POS[:,1]-Offset[1],POS[:,2]-Offset[2],marker='o',s=1, c=curve_color)
+proj_plt.set_xlabel("X [mm]")
+proj_plt.set_ylabel("Y [mm]")
+proj_plt.set_zlabel("Z [mm]")
 
-################################################# Display Plots and Save them
-
-plt.savefig(pathToData.split(".")[0] + ".png")
-print("Saving " + pathToData.split(".")[0] + ".png ...")
-
+################################################# Display Plots
 plt.show()
